e2d09d2e7010695c2a5199259c113b228f0b284d
Aryansh1412 <aryanshjain1412@gmail.com>
2024-07-16 03:21:57 +0530

    add->pro

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,7 @@
     import project2 from "../assets/projects/project-2.jpg";
     import project3 from "../assets/projects/project-3.jpg";
     import project4 from "../assets/projects/project-4.jpg";
+    import project5 from "../assets/projects/project-5.jpg";
 
     export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
@@ -57,15 +58,15 @@
         title: "Portfolio Website",
         image: project3,
         description:
-          "A personal portfolio website showcasing projects, skills, and contact information.",
+          "A real-time code editor application using React, enabling users to write, edit, and execute code collaboratively. WebSocket is used for real-time code sharing and editing",
         technologies: ["HTML", "CSS", "React", "Bootstrap"],
       },
       {
-        title: "Portfolio Website",
-        image: project3,
+        title: "RealTime Code-Editor",
+        image: project5,
         description:
           "A personal portfolio website showcasing projects, skills, and contact information.",
-        technologies: ["HTML", "CSS", "React", "Bootstrap"],
+        technologies: ["React.js", "WebSocket", "CodeMirror", "Node.js(for backend)", "Express.js"],
       },
       // {
       //   title: "Blogging Platform",
